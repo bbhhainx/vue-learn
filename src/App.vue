d951b4dda2aa8f7ac28af0ba095b150508dae8d4
bbhhainx <bbhhainx>
2025-04-28 13:56:00 +0700

    fix

--- src/App.vue
+++ src/App.vue
@@ -6,14 +6,20 @@
   <!-- <ExampleBuilder /> -->
   <!-- <ExampleBuilder2 /> -->
   <!-- <TiptapEditor /> -->
-  <TiptapEditorLib />
+  <div v-if="show">
+    <TiptapEditorLib />
+  </div>
+  <button @click="() => (show = true)">Open</button>
 </template>
 
 <script setup lang="ts">
-import Builder from "./views/Builder.vue";
-import ExampleBuilder from "./views/ExampleBuilder.vue";
-import ExampleBuilder2 from "./views/ExampleBuilder2.vue";
-import Input from "./views/Input.vue";
-import TiptapEditor from "./views/TiptapEditor.vue";
-import TiptapEditorLib from "./views/TiptapEditorLib.vue";
+import { ref } from 'vue'
+import Builder from './views/Builder.vue'
+import ExampleBuilder from './views/ExampleBuilder.vue'
+import ExampleBuilder2 from './views/ExampleBuilder2.vue'
+import Input from './views/Input.vue'
+import TiptapEditor from './views/TiptapEditor.vue'
+import TiptapEditorLib from './views/TiptapEditorLib.vue'
+
+const show = ref(false)
 </script>
